bf4c940f254e4281402c3515df8c1a60b99bb0cc
Michel Erler <7072141+erler@users.noreply.github.com>
2022-05-04 12:20:14 +0200

    Resolver page background animation (#19)
    * adding tailwind
    
    * bg animation
    
    * moving animation to resolver page
    
    * Use unstoppable
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * add link to ink repo
    
    Co-authored-by: Michael Mueller <mich@elmueller.net>
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>

--- frontend/src/Resolver.tsx
+++ frontend/src/Resolver.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
+import squid from "./squid.svg";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { Abi, ContractPromise } from "@polkadot/api-contract";
 import metadata from "./metadata.json";
@@ -37,11 +37,30 @@ const Resolver = () => {
   }, [api, slug]);
   return (
     <div className="App">
-      <div className="top-bar">
-        <img src={logo} className="ink-logo" alt="logo" />
-      </div>
+      {/*
       <div className="slug">Your Slug: {slug}</div>
-      <div className="url">Your Resolved URL: {resolvedUrl}</div>
+      <div className="url">Your Resolved URL: {resolvedUrl}</div> */}
+      <section className="sticky">
+        <div className="squids">
+          <img src={squid} className="squid" alt="logo"/>
+          <img src={squid} className="squid" alt="logo"/>
+          <img src={squid} className="squid" alt="logo"/>
+          <img src={squid} className="squid" alt="logo"/>
+          <img src={squid} className="squid" alt="logo"/>
+          <img src={squid} className="squid" alt="logo"/>
+          <img src={squid} className="squid" alt="logo"/>
+        </div>
+        <img src={squid} className="big-squid" alt="logo"/>
+      </section>
+      <div className='container'>
+        <div className='text-info'>
+          <h1>Shortened with link!</h1>
+          <div className='tag-line'>
+            The unstoppable link shortener build with the <a href="https://github.com/paritytech/ink" target="_blank" rel="noopener noreferrer">ink! smart contract language</a>.
+            {resolvedUrl && `Redirecting to ${resolvedUrl}.`}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
