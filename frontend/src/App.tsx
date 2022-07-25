7828bc5cfe09df225924854b82e59581eca5ca1a
Michel Erler <7072141+erler@users.noreply.github.com>
2022-07-25 18:37:23 +0200

    Links overview (#20)
    * adding components folder
    
    * add headlessui
    
    * Tabs functionality
    
    * remove focus links
    
    * put urls in localStorage
    
    * centering header
    
    * overflow
    
    * Update Header.tsx
    
    * fix local storage errors
    
    Co-authored-by: Andreea Eftene <hi@pixelle.ro>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
 import linkLogo from "./link-logo.svg";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { Abi, ContractPromise } from "@polkadot/api-contract";
 import { formatBalance, formatNumber } from "@polkadot/util";
 import metadata from "./metadata.json";
 import { nanoid } from "nanoid";
+import Header from "./components/Header";
+import LinksOverview from "./components/LinksOverview";
 
 type Estimation = {
   gasRequired: string;
@@ -18,6 +19,11 @@ function App() {
   const [url, setUrl] = useState("");
   const [alias, setAlias] = useState(nanoid(5));
   const [estimation, setEstimation] = useState<Estimation>();
+  const [index, setIndex] = useState();
+
+  const indexFromTabs = (index:any) => {
+    setIndex(index);
+  }
 
   useEffect(() => {
     const wsProvider = new WsProvider("ws://127.0.0.1:9944");
@@ -49,46 +55,60 @@ function App() {
       });
   }, [alias, api, url]);
 
+  const handleLocalStorage = () => {
+    const storedUrls = localStorage.getItem("urls");
+    const urls = storedUrls ? JSON.parse(storedUrls) : [];
+    urls.push(url);
+    localStorage.setItem("urls", JSON.stringify(urls))
+  }
+
   return (
     <div className="App">
-      <div className="top-bar">
-        <img src={logo} className="ink-logo" alt="logo" />
-      </div>
+      <Header indexFromTabs={indexFromTabs} />
       <div className="content">
-        <div className="form-panel">
-          <img src={linkLogo} className="link-logo" alt="logo" />
-          <form action="#">
-            <label htmlFor="url">Your URL</label>
-            <input
-              type="text"
-              id="url"
-              name="url"
-              placeholder="Your URL"
-              value={url}
-              onChange={(e) => {
-                setUrl(e.target.value);
-              }}
-            />
-            <label htmlFor="alias">Your Alias</label>
-            <input
-              type="text"
-              id="alias"
-              name="alias"
-              placeholder="Your Alias"
-              value={alias}
-              onChange={(e) => {
-                setAlias(e.target.value);
-              }}
-            />
-            {estimation && (
-              <div className="estimations">
-                <p>storage deposit: {estimation.storageDeposit}</p>
-                <p>gas required: {estimation.gasRequired}</p>
-              </div>
-            )}
-            <button>Shorten</button>
-          </form>
-        </div>
+        {index === 0
+        ?
+          <div className="form-panel">
+            <img src={linkLogo} className="link-logo" alt="logo" />
+            <form action="#">
+              <label htmlFor="url">Your URL</label>
+              <input
+                type="text"
+                id="url"
+                name="url"
+                placeholder="Your URL"
+                value={url}
+                onChange={(e) => {
+                  setUrl(e.target.value);
+                }}
+              />
+              <label htmlFor="alias">Your Alias</label>
+              <input
+                type="text"
+                id="alias"
+                name="alias"
+                placeholder="Your Alias"
+                value={alias}
+                onChange={(e) => {
+                  setAlias(e.target.value);
+                }}
+              />
+              {estimation && (
+                <div className="estimations">
+                  <p>storage deposit: {estimation.storageDeposit}</p>
+                  <p>gas required: {estimation.gasRequired}</p>
+                </div>
+              )}
+              <button
+                onClick={handleLocalStorage}
+              >
+                Shorten
+              </button>
+            </form>
+          </div>
+        :
+          <LinksOverview />
+      }
       </div>
     </div>
   );
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { Tab } from '@headlessui/react'
+import { useEffect, useState } from 'react'
+import logo from "../logo.svg";
+
+interface Props {
+    indexFromTabs: (index: number) => void;
+}
+
+const Header = ({indexFromTabs}: Props) => {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    useEffect(() => {
+        indexFromTabs(selectedIndex);
+    }, [indexFromTabs, selectedIndex]);
+
+    return (
+        <div className="flex justify-between w-full px-8 py-4">
+            <div className='flex items-center w-32'>
+                <img src={logo} className="ink-logo" alt="logo" />
+            </div>
+            <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+                <Tab.List className="bg-gray-900 p-1 rounded-full">
+                    <Tab 
+                        className={({ selected }) =>
+                        selected ? 'bg-gray-600 text-white hover:bg-gray-600 focus:outline-none' : 'bg-transparent text-gray-500 hover:bg-transparent hover:text-white'
+                    }>
+                        Shorten
+                    </Tab>
+                    <Tab 
+                        className={({ selected }) =>
+                        selected ? 'bg-gray-600 text-white hover:bg-gray-600 focus:outline-none' : 'bg-transparent text-gray-500 hover:bg-transparent hover:text-white'
+                    }>
+                        Your Links
+                    </Tab>
+                </Tab.List>
+            </Tab.Group>
+            <div className='flex items-center justify-end w-32'>
+                <button>Connect</button>
+            </div>  
+        </div>
+    )
+}
+
+export default Header;
