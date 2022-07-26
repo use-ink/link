68c5b995def279df1e0368f388e9731fc74c0b82
Andreea Eftene <42985153+statictype@users.noreply.github.com>
2022-07-26 23:57:17 +0200

    Manage form state with Formik and Yup (#25)
    * switch to Formik
    
    * display better errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,112 +3,91 @@ import "./App.css";
 import linkLogo from "./link-logo.svg";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { Abi, ContractPromise } from "@polkadot/api-contract";
-import { formatBalance, formatNumber } from "@polkadot/util";
 import metadata from "./metadata.json";
-import { nanoid } from "nanoid";
-import Header from "./components/Header";
-import LinksOverview from "./components/LinksOverview";
-
-type Estimation = {
-  gasRequired: string;
-  storageDeposit: string;
-};
+import { Header, LinksOverview, CostEstimations } from "./components";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Estimation } from "./types";
+import {
+  initialValues,
+  contractAddress,
+  endpoint,
+  UrlShortenerSchema,
+} from "./const";
 
 function App() {
   const [api, setApi] = useState<ApiPromise | null>(null);
-  const [url, setUrl] = useState("");
-  const [alias, setAlias] = useState(nanoid(5));
-  const [estimation, setEstimation] = useState<Estimation>();
   const [index, setIndex] = useState();
-
-  const indexFromTabs = (index:any) => {
+  const [contract, setContract] = useState<ContractPromise>();
+  const [estimation, setEstimation] = useState<Estimation>();
+  const indexFromTabs = (index: any) => {
     setIndex(index);
-  }
+  };
 
   useEffect(() => {
-    const wsProvider = new WsProvider("ws://127.0.0.1:9944");
+    const wsProvider = new WsProvider(endpoint);
     ApiPromise.create({ provider: wsProvider }).then((api) => setApi(api));
   }, []);
 
   useEffect(() => {
-    if (!api) return;
+    if (!api || contract) return;
     const abi = api && new Abi(metadata, api.registry.getChainProperties());
-    const contract = new ContractPromise(
-      api,
-      abi,
-      "5DuGo1LGXFs2xW6QGwKLQxRc5wyVsh2FUHCZ4y9wov2Ma9P7"
-    );
-    const params = [{ new: url }, alias];
-    url &&
-      alias &&
-      contract.query["shorten"](
-        "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
-        { gasLimit: -1, storageDepositLimit: null },
-        ...params
-      ).then(({ storageDeposit, gasRequired }) => {
-        storageDeposit && gasRequired
-          ? setEstimation({
-              gasRequired: formatNumber(gasRequired),
-              storageDeposit: formatBalance(storageDeposit.asCharge),
-            })
-          : setEstimation(undefined);
-      });
-  }, [alias, api, url]);
-
-  const handleLocalStorage = () => {
-    const storedUrls = localStorage.getItem("urls");
-    const urls = storedUrls ? JSON.parse(storedUrls) : [];
-    urls.push(url);
-    localStorage.setItem("urls", JSON.stringify(urls))
-  }
+    const c = new ContractPromise(api, abi, contractAddress);
+    setContract(c);
+  }, [api, contract]);
 
   return (
     <div className="App">
       <Header indexFromTabs={indexFromTabs} />
       <div className="content">
-        {index === 0
-        ?
+        {index === 0 ? (
           <div className="form-panel">
             <img src={linkLogo} className="link-logo" alt="logo" />
-            <form action="#">
-              <label htmlFor="url">Your URL</label>
-              <input
-                type="text"
-                id="url"
-                name="url"
-                placeholder="Your URL"
-                value={url}
-                onChange={(e) => {
-                  setUrl(e.target.value);
-                }}
-              />
-              <label htmlFor="alias">Your Alias</label>
-              <input
-                type="text"
-                id="alias"
-                name="alias"
-                placeholder="Your Alias"
-                value={alias}
-                onChange={(e) => {
-                  setAlias(e.target.value);
-                }}
-              />
-              {estimation && (
-                <div className="estimations">
-                  <p>storage deposit: {estimation.storageDeposit}</p>
-                  <p>gas required: {estimation.gasRequired}</p>
-                </div>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={UrlShortenerSchema}
+              onSubmit={(values, { setSubmitting }) => {
+                console.log(values);
+                setSubmitting(false);
+              }}
+            >
+              {({ isSubmitting, isValid, values }) => (
+                <Form>
+                  <div className="group">
+                    <Field type="text" name="url" />
+                    <ErrorMessage
+                      name="url"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="group">
+                    <Field type="text" name="alias" />
+                    <ErrorMessage
+                      name="alias"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="group">
+                    {isValid && contract && (
+                      <CostEstimations
+                        contract={contract}
+                        values={values}
+                        estimation={estimation}
+                        setEstimation={setEstimation}
+                      />
+                    )}
+                  </div>
+                  <button type="submit" disabled={isSubmitting}>
+                    Shorten
+                  </button>
+                </Form>
               )}
-              <button
-                onClick={handleLocalStorage}
-              >
-                Shorten
-              </button>
-            </form>
+            </Formik>
           </div>
-        :
+        ) : (
           <LinksOverview />
-      }
+        )}
       </div>
     </div>
   );
