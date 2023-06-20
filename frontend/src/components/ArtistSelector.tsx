a5baef570b7f8ed26c1ed9696c499dc7e81e7cee
Bobo <bobo.kovacevic@gmail.com>
2023-06-20 13:53:51 +0200

    Fix for option rendering on iOS (#41)
    * Fix for option items rendering on iOS
    
    * select height fix

--- frontend/src/components/ArtistSelector.tsx
+++ frontend/src/components/ArtistSelector.tsx
@@ -22,71 +22,25 @@ export const ArtistSelector = ({ values }: { values: PinkValues }) => {
         style={{ display: "block" }}
         title="Select an Artist style. You can also enter any other Artist style in the prompt field."
       >
-        <option value="" disabled >Select an Artist style</option>
-        <option
-          value={artistStyles.none.name}
-          label="No Artist selected"
-        ></option>
-        <option
-          value={artistStyles.kusama.name}
-          label="Yayoi Kusama"
-        ></option>
-        <option
-          value={artistStyles.amano.name}
-          label="Yoshitaka Amano"
-        ></option>
-        <option
-          value={artistStyles.takashi.name}
-          label="Takashi Murakami"
-        ></option>
-        <option
-          value={artistStyles.hokusai.name}
-          label="Katsushika Hokusai"
-        ></option>
-        <option
-          value={artistStyles.picasso.name}
-          label="Pablo Picasso"
-        ></option>
-        <option
-          value={artistStyles.gogh.name}
-          label="Vincent van Gogh"
-        ></option>
-        <option
-          value={artistStyles.dali.name}
-          label="Salvador Dali"
-        ></option>
-        <option
-          value={artistStyles.pollock.name}
-          label="Jackson Pollock"
-        ></option>
-        <option
-          value={artistStyles.warhol.name}
-          label="Andy Warhol"
-        ></option>
-        <option
-          value={artistStyles.matisse.name}
-          label="Henri Matisse"
-        ></option>
-        <option
-          value={artistStyles.kandinsky.name}
-          label="Wassily Kandinsky"
-        ></option>
-        <option
-          value={artistStyles.munch.name}
-          label="Edvard Munch"
-        ></option>
-        <option
-          value={artistStyles.banksy.name}
-          label="Banksy"
-        ></option>
-        <option
-          value={artistStyles.haring.name}
-          label="Keith Haring"
-        ></option>
-        <option
-          value={artistStyles.basquiat.name}
-          label="Jean-Michel Basquiat"
-        ></option>
+        <option value="" disabled>
+          Select an Artist style
+        </option>
+        <option value={artistStyles.none.name}>No Artist selected</option>
+        <option value={artistStyles.kusama.name}>Yayoi Kusama</option>
+        <option value={artistStyles.amano.name}>Yoshitaka Amano</option>
+        <option value={artistStyles.takashi.name}>Takashi Murakami</option>
+        <option value={artistStyles.hokusai.name}>Katsushika Hokusai</option>
+        <option value={artistStyles.picasso.name}>Pablo Picasso</option>
+        <option value={artistStyles.gogh.name}>Vincent van Gogh</option>
+        <option value={artistStyles.dali.name}>Salvador Dali</option>
+        <option value={artistStyles.pollock.name}>Jackson Pollock</option>
+        <option value={artistStyles.warhol.name}>Andy Warhol</option>
+        <option value={artistStyles.matisse.name}>Henri Matisse</option>
+        <option value={artistStyles.kandinsky.name}>Wassily Kandinsky</option>
+        <option value={artistStyles.munch.name}>Edvard Munch</option>
+        <option value={artistStyles.banksy.name}>Banksy</option>
+        <option value={artistStyles.haring.name}>Keith Haring</option>
+        <option value={artistStyles.basquiat.name}>Jean-Michel Basquiat</option>
       </select>
     </div>
   );
--- frontend/src/components/ModelSelector.tsx
+++ frontend/src/components/ModelSelector.tsx
@@ -22,39 +22,23 @@ export const ModelSelector = ({ values }: { values: PinkValues }) => {
         style={{ display: "block" }}
         title="Select an AI model. All models are Open Source. The best resolution is with the Stable Diffusion v2.1 model and that is 768 × 768px. Other models will generate 512 × 512px image."
       >
-        <option value="" disabled selected>Select AI model</option>
-        <option
-          value={aiModels.stablediffusion.name}
-          label="Stable Diffusion v2.1"
-        ></option>
-        <option
-          value={aiModels.anything.name}
-          label="Anything"
-        ></option>
-        <option
-          value={aiModels.openjourney.name}
-          label="Open Journey"
-        ></option>
-        <option
-          value={aiModels.anythingmidjourney.name}
-          label="Anything MidJourney"
-        ></option>
-        <option
-          value={aiModels.pokemondiffusers.name}
-          label="Pokemon Diffusers"
-        ></option>
-        <option
-          value={aiModels.arcanediffusion.name}
-          label="Arcane Diffusion"
-        ></option>
-        <option
-          value={aiModels.eimisanime.name}
-          label="Eimis Anime"
-        ></option>
-        <option
-          value={aiModels.waifu.name}
-          label="Waifu"
-        ></option>
+        <option value="" disabled selected>
+          Select AI model
+        </option>
+        <option value={aiModels.stablediffusion.name}>
+          Stable Diffusion v2.1
+        </option>
+        <option value={aiModels.anything.name}>Anything</option>
+        <option value={aiModels.openjourney.name}>Open Journey</option>
+        <option value={aiModels.anythingmidjourney.name}>
+          Anything MidJourney
+        </option>
+        <option value={aiModels.pokemondiffusers.name}>
+          Pokemon Diffusers
+        </option>
+        <option value={aiModels.arcanediffusion.name}>Arcane Diffusion</option>
+        <option value={aiModels.eimisanime.name}>Eimis Anime</option>
+        <option value={aiModels.waifu.name}>Waifu</option>
       </select>
     </div>
   );
