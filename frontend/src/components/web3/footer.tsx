4b92a7b5d09a71f50e3e2d1594a5a74f0ce773ec
peetzweg/ <phil.czek@gmail.com>
2024-04-26 13:34:41 +0200

    adds popnetwork to footer

--- frontend/src/components/web3/footer.tsx
+++ frontend/src/components/web3/footer.tsx
@@ -4,12 +4,17 @@ export const Footer = () => {
   return (
     <footer className="mt-4 flex w-full flex-col items-center justify-center gap-2">
       <div>
-        <a href="https://use.ink/" target="_blank" rel="noreferrer">
-          <div className="flex flex-row gap-2 text-black opacity-65 hover:cursor-pointer hover:opacity-80">
+        <div className="flex flex-row gap-2 text-black opacity-65 hover:cursor-pointer hover:opacity-80">
+          <a href="https://use.ink/" target="_blank" rel="noreferrer">
             <div>Proudly built with</div>
+          </a>
+          <a href="https://use.ink/" target="_blank" rel="noreferrer">
             <img src={Logo} alt="Squid" className="h-5" />
-          </div>
-        </a>
+          </a>
+          <a href="https://popnetwork.xyz/">
+            <div>& deployed on POP Network</div>
+          </a>
+        </div>
       </div>
 
       <div className="flex w-full flex-row flex-wrap items-center justify-center gap-4">
@@ -31,7 +36,7 @@ export const Footer = () => {
           GitHub <HiOutlineExternalLink />
         </a>
         <a
-          href={"https://use.ink/faucet/"}
+          href={"https://popnetwork.xyz/"}
           target="_blank"
           rel="noreferrer"
           className="flex items-center justify-center gap-1 text-center text-sm text-gray-600 hover:text-gray-800"
